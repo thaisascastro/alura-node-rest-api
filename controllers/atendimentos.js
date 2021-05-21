352beb43ea54d04d85f9f15a96524318e205093c
thaisascastro <thaisascastro@gmail.com>
2021-05-21 16:33:29 -0300

    realizando um post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,7 @@
 module.exports = app => {
-  app.get('/atendimento', (req, res) => res.send("Você está na rota atendimento!"));
+  //trazer dados
+  app.get('/atendimento', (req, res) => res.send("Você está na rota atendimento e realizando um GET!"));
+
+  // enviar dados
+  app.post('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos e realizando um POST!"))
 }
