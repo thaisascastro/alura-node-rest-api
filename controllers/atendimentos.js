0f98d999a2e7eda314d63005dbd1131f095470d5
thaisascastro <thaisascastro@gmail.com>
2021-05-21 16:58:33 -0300

    rota para criar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,9 @@
 module.exports = app => {
   //trazer dados
-  app.get('/atendimento', (req, res) => res.send("Você está na rota atendimento e realizando um GET!"));
+  app.get('/atendimentos', (req, res) => res.send("Você está na rota atendimento e realizando um GET!"));
 
   // enviar dados
-  app.post('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos e realizando um POST!"))
+  app.post('/atendimentos', (req, res) => {
+    
+    res.send("Você está na rota de atendimentos e realizando um POST!")})
 }
