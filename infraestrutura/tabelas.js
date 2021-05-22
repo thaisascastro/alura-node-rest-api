69f90b1f1978a30eaf9936fe4ff790f8ee9b3537
thaisascastro <thaisascastro@gmail.com>
2021-05-22 17:29:15 -0300

    IF NOT EXIT

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -3,10 +3,20 @@ class Tabelas {
     // sempre passar o this.conexao = conexao para obtiver.
     //console.log("Tabelas foram chamadas") 
     this.conexao = conexao
+
+    this.criarAtendimento()
   }
 
+  // colocar IF NOT EXIST para não criar sempre uma tabela
   criarAtendimento() {
-    const sql = 'CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+    const sql = `CREATE TABLE IF NOT EXIST Atendimentos 
+    (id int NOT NULL AUTO_INCREMENT, 
+      cliente varchar(50) NOT NULL, 
+      pet varchar(20), 
+      servico varchar(20) NOT NULL, 
+      status varchar(20) NOT NULL, 
+      observacoes text, 
+      PRIMARY KEY(id))`
 
       this.conexao.query(sql, (erro) => {
         if(erro) {
