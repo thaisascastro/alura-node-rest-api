df2f16f2dbc853b42053581f873643bdc96edf0e
thaisascastro <thaisascastro@gmail.com>
2021-05-23 17:10:13 -0300

    adicionando data nas tabelas

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,12 +1,17 @@
+const moment = require('moment');
+
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
   adiciona(atendimento) {
+    const dataCriacao = new Date()
+    const atendimentoDatado = {...atendimento, dataCriacao}
+
 // criando nossa query 
     const sql = 'INSERT INTO Atendimentos SET ?'
     // ? significa o que a gente vai adicionar na tabela
 
-    conexao.query(sql, atendimento, (erro, resultados) => {
+    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
       if(erro) {
         console.log(erro)
       }
