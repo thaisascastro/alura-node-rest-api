0c7c12fc825e20240fee13b11f2fe42170ebf912
thaisascastro <thaisascastro@gmail.com>
2021-05-23 19:25:41 -0300

    verbos get

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,68 +1,65 @@
-const moment = require('moment');
+const moment = require("moment");
 
-const conexao = require('../infraestrutura/conexao')
+const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
-
   adiciona(atendimento, res) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
+    const data = moment(atendimento.data, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
 
-    const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-    const clienteEhValido = atendimento.cliente.length >= 4
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 4;
 
     const validacoes = [
       {
-        nome: 'data',
+        nome: "data",
         valido: dataEhValida,
-        mensagem: 'Data deve ser maior ou igual a data atual.'
+        mensagem: "Data deve ser maior ou igual a data atual.",
       },
       {
-        nome: 'cliente',
+        nome: "cliente",
         valido: clienteEhValido,
-        mensagem: 'Cliente deve ter pelo menos cinco caracteres.'
-      }
-    ]
+        mensagem: "Cliente deve ter pelo menos cinco caracteres.",
+      },
+    ];
 
-    const erros = validacoes.filter(campo => !campo.valido)
-    const existemErros = erros.length
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
 
-    if(existemErros) {
-      res.status(400).json(erros)
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      res.status(201).json(resultados);
     }
-    else {
-      res.status(201).json(resultados)
-    }
 
+    const atendimentoDatado = { ...atendimento, dataCriacao, data };
 
-    const atendimentoDatado = {...atendimento, dataCriacao, data}
-
-// criando nossa query 
-    const sql = 'INSERT INTO Atendimentos SET ?'
+    // criando nossa query
+    const sql = "INSERT INTO Atendimentos SET ?";
     // ? significa o que a gente vai adicionar na tabela
 
     conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if(erro) {
-        res.status(400).json(erro)
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(201).json(resultados);
       }
-      else {
-        res.status(201).json(resultados)
-      }
-    })
+    });
   }
+
   lista(res) {
-    const sql = 'SELECT * FROM Atendimentos'
-  
+    const sql = "SELECT * FROM Atendimentos";
+
     conexao.query(sql, (erro, resultados) => {
-        if(erro) {
-        res.status(400).json(erro)
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
       }
-        else {
-        res.status(200).json(resultados)
-        }
-      })
-    }
+    });
   }
+}
 
-
-module.exports = new Atendimento
+module.exports = new Atendimento();
