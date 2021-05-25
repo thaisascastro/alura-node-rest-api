63381c765111e0ca55126458d224dc5f64b9bb50
thaisascastro <thaisascastro@gmail.com>
2021-05-25 18:32:43 -0300

    deletando dados

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -91,6 +91,18 @@ class Atendimento {
       }
     });
   }
+
+  deleta(id, res) {
+    const sql = "DELETE FROM Atendimentos WHERE id=?";
+
+    conexao.query(sql, id, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ id });
+      }
+    });
+  }
 }
 
 module.exports = new Atendimento();
